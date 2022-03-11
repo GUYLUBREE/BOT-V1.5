353e58831cf8587d4657fd21311c5783f6d85613
GUYLUBREE <asepasepanjay@gmail.com>
2022-03-11 09:39:21 +0700

    BOT V.1.5

---
+++ config.js
@@ -0,0 +1,60 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': '805a6c3fa9',
+}
+
+// Other
+global.owner = ['6285741857464','6283127014833','6281365814200']
+global.pemilik = ['6283127014833']
+global.premium = ['6281365814200']
+global.pengguna = 'Nama Lu Isi'
+global.botnma = 'Lycho Bot'
+global.footer = 'KahfzXzy' //FooterText Biar Di Bawah Nya Ada Text 
+global.web = 'https://youtube.com/c/KahfzXzy' // Ubah Sama Lu Web Nya Bebas 
+global.web2 = 'https://github.com/K4HFZ' //Ubah Jga Bebas Sama lu 
+global.ganti = 'Y O U T U B E ME' //Edit Terserah Lu Mau Apa
+global.ganti2 = 'Github'
+global.ownernma = 'KahfzXzy'
+global.packname = 'Lycho Bot'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./media/Lycho.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
